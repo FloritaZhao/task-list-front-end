6d103d9145a39107bb3b049f29ab0f4de1b8d20a
FloritaZhao <feifeilan@hotmail.com>
2025-06-11 12:54:44 -0700

    updated wave 02 - background color, title, completed-add on

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -13,7 +13,7 @@ const Task = ({ id, title, isComplete }) => {
         className={`tasks__item__toggle ${buttonClass}`}
         onClick={() => setComplete(!complete)}
       >
-        {title}
+        {complete ? `${title} - completed` : title}
       </button>
       <button className="tasks__item__remove button">x</button>
     </li>
